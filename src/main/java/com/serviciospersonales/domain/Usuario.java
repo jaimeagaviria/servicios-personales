ba8f5ffb6680c4c36e48a3c56be5bfef9d555735
jgaviria-at-514287058315 <0WwJBox0e/NOjrJV/Cr3P0S4b913zkz98yqbtyj6CBQ=>
2017-11-29 16:07:16 -0500

    JG: Siembra inicial del proyecto

--- src/main/java/com/serviciospersonales/domain/Usuario.java
+++ src/main/java/com/serviciospersonales/domain/Usuario.java
@@ -1,6 +1,9 @@
 package com.serviciospersonales.domain;
 
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
 import javax.persistence.*;
 
 @Entity
@@ -12,11 +15,35 @@ public class Usuario {
     @Column(name = "usuario_id")
     private Integer usuarioID;
 
+    @NotBlank
+    @Email
+    @Column(name = "email", length = 200, nullable = false)
+    private String email;
+
+    @Column(name = "activo", nullable = false)
+    private Boolean activo = false;
+
     public Integer getUsuarioID() {
         return usuarioID;
     }
 
     public void setUsuarioID(Integer usuarioID) {
         this.usuarioID = usuarioID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
     }
 }
